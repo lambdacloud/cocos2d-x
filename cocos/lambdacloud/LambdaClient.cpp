e2a803a09a18cc857c3713724b663bc1875b7ad5
sky4star <382103956@qq.com>
2014-12-26 14:24:48 +0800

    LambdaClinet init check, support MAC and IOS (v2)
    iteration 2
    update some comments

--- cocos/lambdacloud/LambdaClient.cpp
+++ cocos/lambdacloud/LambdaClient.cpp
@@ -1,10 +1,29 @@
-//
-//  LambdaClient.cpp
-//  cocos2d_libs
-//
-//  Created by sky4star on 14/12/14.
-//
-//
+/*
+ Copyright (c) 2014, LambdaCloud
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include "LambdaClient.h"
 
@@ -59,7 +78,7 @@ void LambdaClient::writeLog(const std::string& log, std::vector<std::string> tag
     request->setResponseCallback(CC_CALLBACK_2(LambdaClient::onHttpRequestCompleted, this));
     cocos2d::network::HttpClient::getInstance()->send(request);
     
-    //TODO why shall we call release explicitly?
+    // Release request
     request->release();
     return;
 }
